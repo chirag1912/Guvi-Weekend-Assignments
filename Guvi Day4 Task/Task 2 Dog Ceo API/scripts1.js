04515e09052705b5225281e4e436ad6f9e6efc32
chirag1912 <chiragbrajpal@gmail.com>
2020-07-28 23:44:40 +0530

    Add files via upload

---
+++ Guvi Day4 Task/Task 2 Dog Ceo API/scripts1.js
@@ -0,0 +1,11 @@
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest()
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://dog.ceo/api/breeds/list/all', true)
+request.send();
+request.onload = function() {
+  // Begin accessing JSON data here
+var data = JSON.parse(this.response)
+console.log(data);
+}
