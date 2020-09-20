25f9e91b8461edc249f7d9c5d32119d121737f82
chirag1912 <chiragbrajpal@gmail.com>
2020-09-20 23:57:36 +0530

    Add files via upload

---
+++ Class19 Assignment PetShop/Petshop.js
@@ -0,0 +1,69 @@
+var PetsAvailability = /** @class */ (function () {
+    function PetsAvailability() {
+        this.DogCounter = 0;
+        this.CatCounter = 0;
+        this.ParrotCounter = 0;
+    }
+    PetsAvailability.prototype.insertPet = function (_kindofPet, _character, _historyOfPet) {
+        this.kindofPet = _kindofPet;
+        this.character = _character;
+        this.historyOfPet = _historyOfPet;
+        if (_kindofPet == "Dog") {
+            // this.PetsCount.DogCounter+=1;
+            this.DogCounter += 2;
+        }
+        if (_kindofPet == "Cat") {
+            // this.PetsCount.CatCounter+=1;
+            this.CatCounter += 1;
+        }
+        if (_kindofPet == "Parrot") {
+            // this.PetsCount.ParrotCounter+=1;
+            this.ParrotCounter += 1;
+        }
+    };
+    PetsAvailability.prototype.statusofPet = function (_petsofChoice) {
+        for (var i = 0; i < _petsofChoice.length; i++) {
+            if (_petsofChoice[i] == this.historyOfPet.familyOfPet) { //Comparing if requested is available;
+                console.log("Breed of" + _petsofChoice[i] + " is available as requested");
+            }
+        }
+    };
+    PetsAvailability.prototype.counterCheck = function () {
+        console.log("Counter of the Animals is as follows" + " Dogs:" + this.DogCounter + " Cats:" + this.CatCounter + " Parrots:" + this.ParrotCounter);
+    };
+    PetsAvailability.prototype.availabilityCheckofRequests = function (_PetsofChoice) {
+        var counter = 0;
+        for (var i = 0; i < _PetsofChoice.length; i++) {
+            if (_PetsofChoice[i] == this.historyOfPet.familyOfPet) {
+                counter += 1;
+                this.availabilityCounter = counter;
+            }
+        }
+        console.log("Number of choice requested by the users with the number of choice smatching in the Availability are: " + counter);
+    };
+    return PetsAvailability;
+}());
+var PetsRequest = /** @class */ (function () {
+    function PetsRequest() {
+    }
+    PetsRequest.prototype.enquirePet = function (_petsofchoice, _petHabitsdata) {
+        this.petHabitsdata = _petHabitsdata;
+        this.petsofChoice = _petsofchoice;
+        console.log("Choice of Pets Required by Users are:");
+        for (var i in _petsofchoice) {
+            console.log(_petsofchoice[i]);
+        }
+        return _petsofchoice;
+    };
+    return PetsRequest;
+}());
+var obj = new PetsAvailability();
+obj.insertPet("Dog", "trained", { familyOfPet: "Labrador", colorOfPet: "Brown", eatingHabits: "Vegetarian", tagNumber: 2301 });
+obj.insertPet("Dog", "Not-trained", { familyOfPet: "Labrador", colorOfPet: "Brown", eatingHabits: "Vegetarian", tagNumber: 2302 });
+obj.insertPet("Cat", "tamed", { familyOfPet: "Labrador", colorOfPet: "Brown", eatingHabits: "Vegetarian", tagNumber: 3009 });
+obj.insertPet("Parrot", "trained", { familyOfPet: "Labrador", colorOfPet: "Brown", eatingHabits: "Vegetarian", tagNumber: 40041 });
+var obj1 = new PetsRequest();
+var PetsChoice = obj1.enquirePet(["Labrador", "Chihuahua", "Alsasian", "Hound", "Roosevelt"]);
+obj.statusofPet(PetsChoice);
+obj.counterCheck();
+obj.availabilityCheckofRequests(PetsChoice);
---
+++ Class19 Assignment PetShop/Petshop.ts
@@ -0,0 +1,118 @@
+interface history{
+    familyOfPet: string;
+    colorOfPet: string;
+    eatingHabits: string;
+    tagNumber: number;
+}
+
+// interface Counter{
+//     DogCounter:number;
+//     CatCounter:number;
+//     ParrotCounter:number;
+// }
+
+interface petHabits{
+    familyOfPet: string;
+    eatingHabits: number;
+    colorOfPet: string;
+    tagNumber: number;
+}
+
+class PetsAvailability{                     //Availability Class
+    //Pets available for adoption with characteristic nad his history;
+    kindofPet: string;
+    character: string;
+    historyOfPet: history;
+    availabilityCounter: number;
+    DogCounter:number;
+    CatCounter:number;
+    ParrotCounter:number;
+
+    constructor(){
+        this.DogCounter=0;
+        this.CatCounter=0;
+        this.ParrotCounter=0;
+    }
+    
+    insertPet(_kindofPet:string,_character:string, _historyOfPet:history){
+        this.kindofPet=_kindofPet;
+        this.character=_character;
+        this.historyOfPet=_historyOfPet;
+
+        if(_kindofPet=="Dog"){                                 
+            // this.PetsCount.DogCounter+=1;
+            this.DogCounter+=1;
+        }   
+        if(_kindofPet=="Cat"){
+            // this.PetsCount.CatCounter+=1;
+            this.CatCounter+=1; 
+        } 
+        if(_kindofPet=="Parrot"){
+            // this.PetsCount.ParrotCounter+=1;
+            this.ParrotCounter+=1;
+        } 
+
+
+    }
+
+    statusofPet(_petsofChoice:string[]){                         //To find these pets in the collection if available or not;
+        for(let i=0;i<_petsofChoice.length;i++){
+            if(_petsofChoice[i]==this.historyOfPet.familyOfPet){                            //Comparing if requested is available;
+                console.log("Breed of"+_petsofChoice[i]+" is available as requested");
+            }
+        }
+
+    }
+
+    counterCheck(){
+        console.log("Counter of the Animals is as follows"+" Dogs:"+this.DogCounter+" Cats:"+this.CatCounter+" Parrots:"+this.ParrotCounter);
+    }
+
+    availabilityCheckofRequests(_PetsofChoice:string[]){
+        let counter=0;
+        for(let i=0;i<_PetsofChoice.length;i++){
+            if(_PetsofChoice[i]==this.historyOfPet.familyOfPet){
+                counter+=1;
+                this.availabilityCounter=counter;
+            }
+        }   
+        console.log("Number of choice requested by the users with the number of choice smatching in the Availability are: "+counter);
+    }
+    
+
+}
+
+class PetsRequest{
+    //Enquiries for the Pet
+    petHabitsdata:petHabits;
+    petsofChoice: string[];
+
+    enquirePet(_petsofchoice,_petHabitsdata?:petHabits){        //insertMethod
+        this.petHabitsdata=_petHabitsdata;
+        this.petsofChoice=_petsofchoice;
+        console.log("Choice of Pets Required by Users are:")
+        for(let i in _petsofchoice){
+            console.log(_petsofchoice[i]);
+        }
+        return _petsofchoice;
+    }
+
+}
+
+let obj=new PetsAvailability();
+obj.insertPet("Dog","trained",{familyOfPet: "Labrador", colorOfPet: "Brown", eatingHabits: "Vegetarian", tagNumber: 2301});
+obj.insertPet("Dog","Not-trained",{familyOfPet: "Labrador", colorOfPet: "Brown", eatingHabits: "Vegetarian", tagNumber: 2302});
+obj.insertPet("Cat","tamed",{familyOfPet: "Labrador", colorOfPet: "Brown", eatingHabits: "Vegetarian", tagNumber: 3009});
+obj.insertPet("Parrot","trained",{familyOfPet: "Labrador", colorOfPet: "Brown", eatingHabits: "Vegetarian", tagNumber: 40041});
+
+
+let obj1=new PetsRequest();
+let PetsChoice=obj1.enquirePet(["Labrador","Chihuahua","Alsasian","Hound","Roosevelt"]);        
+
+obj.statusofPet(PetsChoice);
+
+obj.counterCheck();
+
+obj.availabilityCheckofRequests(PetsChoice);            
+
+
